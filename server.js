ed6c570fc857a7c5cfdc0d224e95febf6803c299
David Eliason <davethemaker@gamil.com>
2017-06-03 15:35:33 -0700

    successful use of dotenv module to store mongoLab credentials, saves form data to db

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+require('dotenv').config()
+
+
+// use MongoLab for cloud-hosted db
+MongoClient.connect('mongodb://'+ process.env.DB_USER +':' + process.env.DB_PASS + '@ds161551.mlab.com:61551/inspiredwords', (err,database) => {
+   if(err) return console.log(err)
+    db = database;
+
+    app.listen(8080,function(){
+        console.log("server up at 8080");
+    });
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,8 +23,12 @@ app.get('/',(req,res) => {
 
 app.post('/quotes', (req,res) => {
     console.log(req.body);
+
+    db.collection('quotes').save(req.body, (err,result) => {
+        if(err) return console.log(err)
+
+        console.log('saved to db');
+        res.redirect('/');
+    });
 });
 
-app.listen(8080,function(){
-    console.log("server up at 8080");
-});
