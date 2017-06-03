298b595ea8245faa4f374668b30eccf33181fa96
David Eliason <davethemaker@gamil.com>
2017-06-03 14:30:47 -0700

    add body-parser module to parse posted form field data

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/quotes', (req,res) => {
+    console.log(req.body);
 });
 
 app.listen(8080,function(){
