c7277e54c42a78953f8a66ce351e540d2b711e57
David Eliason <davethemaker@gamil.com>
2017-06-04 11:20:50 -0700

    update env var for mongoDB

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // get server to read JSON data
 
 // use MongoLab for cloud-hosted db
-MongoClient.connect('mongodb://'+ process.env.DB_USER +':' + process.env.DB_PASS + '@ds161551.mlab.com:61551/inspiredwords', (err,database) => {
+MongoClient.connect('mongodb://'+ process.env.PROD_MONGODB, (err,database) => {
    if(err) return console.log(err)
     db = database;
 
