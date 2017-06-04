e49e2c4354b0babda99cd42d58729ffed96accf0
David Eliason <davethemaker@gamil.com>
2017-06-03 19:51:13 -0700

    add eventListner for delete button to be clicked, grab value, invoke fetch with delete method

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 
 var replaceLastQuote = document.getElementById('replaceLastQuote');
+var deleteQuote = document.getElementById('deleteQuote');
 
 replaceLastQuote.addEventListener('click',() => {
     console.log("submit button was clicked");
@@ -17,4 +18,22 @@ replaceLastQuote.addEventListener('click',() => {
        console.log(data)
        window.location.reload(true)
    })
+});
+
+deleteQuote.addEventListener('click',() => {
+    fetch('quotes',{
+        method: 'delete',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            // grab the author's name value from field
+            'author': document.getElementById("delete_author").value
+        })
+     }).then(res => {
+         if(res.ok) return res.json()
+  }).then(data => {
+      console.log(data)
+      window.location.reload()
+  })
 });
